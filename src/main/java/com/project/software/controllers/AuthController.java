47990ca26aa70158355c01332896942117865ca2
Ernest Agyemang <eagyemang032@st.ug.edu.gh>
2024-04-03 16:09:19 +0100

    updates

--- src/main/java/com/project/software/controllers/AuthController.java
+++ src/main/java/com/project/software/controllers/AuthController.java
@@ -3,15 +3,11 @@ package com.project.software.controllers;
 import com.project.software.dto.AuthRequestDTO;
 import com.project.software.dto.JwtResponseDTO;
 import com.project.software.dto.UserRequestDto;
-import com.project.software.exceptions.NotFoundException;
 import com.project.software.jwt.JwtServiceImpl;
 import com.project.software.models.User;
 import com.project.software.services.implementations.UserDetailsServiceImpl;
 import com.project.software.services.implementations.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +19,6 @@ import java.util.List;
 @RequestMapping("api/v1/auth")
 public class AuthController {
     private final JwtServiceImpl jwtService;
-    private final AuthenticationManager authenticationManager;
     private final UserDetailsServiceImpl userDetailsService;
     private final UserServiceImpl userService;
 
